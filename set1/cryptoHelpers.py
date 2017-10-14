82ed7f4745371f4a2f62b262b1901f413281786a
Kyle Roarty <kyleroarty1716@gmail.com>
2017-10-13 22:17:33 -0500

    ch: implement hamming distance using xor
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- set1/cryptoHelpers.py
+++ set1/cryptoHelpers.py
@@ -8,3 +8,24 @@ def asciiToHex(ascii_str):
 
 def neLines(f):
     return filter(None, (line.strip() for line in f))
+
+def hexToBinary(hex_str):
+    return format(int(hex_str, 16), "0{}b".format(len(hex_str)*4))
+
+def hammingDist(hex_1, hex_2):
+    try:
+        int(hex_1, 16)
+    except ValueError:
+        hex_1 = asciiToHex(hex_1)
+
+    try:
+        int(hex_2, 16)
+    except ValueError:
+        hex_2 = asciiToHex(hex_2)
+
+    bin_1 = hexToBinary(hex_1)
+    bin_2 = hexToBinary(hex_2)
+
+    bin_xor = format(int(bin_1, 2) ^ int(bin_2, 2), '0{}b'.format(len(bin_1)))
+
+    return bin_xor.count('1')
