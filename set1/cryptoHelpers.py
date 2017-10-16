ba07d30c35b4c82493e943695a1f773cf89eca2a
Kyle Roarty <kyleroarty1716@gmail.com>
2017-10-16 11:26:05 -0500

    ch: add bin->hex function; fixedXOR: remove dupe code
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- set1/cryptoHelpers.py
+++ set1/cryptoHelpers.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
+
+def neLines(f):
+    return filter(None, (line.strip() for line in f))
+
 def asciiToHex(ascii_str):
     hex_str = ''
     for char in ascii_str:
         hex_str += format(ord(char), '02x')
     return hex_str
 
-def neLines(f):
-    return filter(None, (line.strip() for line in f))
-
 def hexToBinary(hex_str):
     return format(int(hex_str, 16), "0{}b".format(len(hex_str)*4))
+
+def binaryToHex(bin_str):
+    if len(bin_str)/4 == len(bin_str)//4:
+        return format(int(bin_str, 2), "0{}x".format(len(bin_str)//4))
+    else:
+        # ValueError occurs when bin_str isn't divisible by 4
+        return -1
 
 def isHex(str_1):
     try:
@@ -21,6 +29,9 @@ def isHex(str_1):
     return True
 
 def hammingDist(hex_1, hex_2):
+    return xorHex(hex_1, hex_2).count('1')
+
+def xorHex(hex_1, hex_2):
 
     hex_1 = hex_1 if isHex(hex_1) else asciiToHex(hex_1)
     hex_2 = hex_2 if isHex(hex_2) else asciiToHex(hex_2)
@@ -28,6 +39,6 @@ def hammingDist(hex_1, hex_2):
     bin_1 = hexToBinary(hex_1)
     bin_2 = hexToBinary(hex_2)
 
-    bin_xor = format(int(bin_1, 2) ^ int(bin_2, 2), '0{}b'.format(len(bin_1)))
+    return format(int(bin_1, 2) ^ int(bin_2, 2), '0{}b'.format(len(bin_1)))
 
-    return bin_xor.count('1')
+
--- set1/fixedXOR.py
+++ set1/fixedXOR.py
@@ -1,64 +1,12 @@
-
-HEX_CHARS = '0123456789abcdef'
-
-def _hexCharToBinary(char):
-    int_char = int(char, 16)
-    bit_arr = []
-
-    for i in range(0, 4):
-        bit_arr.insert(0, int_char & 1)
-        int_char = int_char >> 1
-
-    return bit_arr
-
-def _hexStrToBinary(hex_str):
-    ret_arr = []
-    for c in hex_str:
-        ret_arr += _hexCharToBinary(c)
-
-    return ret_arr
-
-def _binStrXOR(bin_1, bin_2):
-    ret_arr = []
-
-    for i in range(0, len(bin_1)):
-        ret_arr.append(bin_1[i] ^ bin_2[i])
-
-    return ret_arr
-
-def _binStrToHexChar(bin_1):
-    hex_char = 0
-
-    for c in bin_1:
-        hex_char = (hex_char << 1) | c
-
-    return HEX_CHARS[hex_char]
-
-def _binStrToHex(bin_1):
-    ret_arr = []
-
-    while len(bin_1) is not 0:
-        sub_bin_str = bin_1[:4]
-        ret_arr.append(_binStrToHexChar(sub_bin_str))
-        bin_1 = bin_1[4:]
-
-    return ''.join(ret_arr)
+import cryptoHelpers as ch
 
 def fixedXOR(hex_1, hex_2):
-    try:
-        int(hex_1, 16)
-        int(hex_2, 16)
-    except ValueError as e:
-        print(e)
-        return -1
+    hex_1 = hex_1 if ch.isHex(hex_1) else ch.asciiToHex(hex_1)
+    hex_2 = hex_2 if ch.isHex(hex_2) else ch.asciiToHex(hex_2)
 
     if len(hex_1) is not len(hex_2):
-        return -2
+        return -1
 
+    xor_hex_bits = ch.xorHex(hex_1, hex_2)
 
-    hex_bits_1 = _hexStrToBinary(hex_1)
-    hex_bits_2 = _hexStrToBinary(hex_2)
-
-    xor_hex_bits = _binStrXOR(hex_bits_1, hex_bits_2)
-
-    return _binStrToHex(xor_hex_bits)
+    return ch.binaryToHex(xor_hex_bits)
