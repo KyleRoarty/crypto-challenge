faabc92352e7f360f536431892bdafb2d55c99e3
Kyle Roarty <kyleroarty1716@gmail.com>
2017-10-17 09:17:06 -0500

    Set 1; challenge 4: completed!
    +Adds spaces to the LUT for character frequency
    +Divide chi_sq by str_len to "normalize"
    
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- set1/findEncrypt.py
+++ set1/findEncrypt.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
 from singleXOR import singleByteDecrypt
-
-def neLines(f):
-    return filter(None, (line.strip() for line in f))
+import cryptoHelpers as ch
 
 def detect(filename):
     f_point = open(filename, 'r')
 
-    for line in neLines(f_point):
+    min_score = 1e309
+    decrypt_str = ''
+
+    for line in ch.neLines(f_point):
         x = singleByteDecrypt(line)
-        if x is not None and x is not '':
+        if x is not None and x[0] is not '':
             print(x)
+            if x[1] < min_score:
+                decrypt_str = x[0]
+                min_score = x[1]
+
+    return decrypt_str
--- set1/singleXOR.py
+++ set1/singleXOR.py
@@ -3,51 +3,45 @@
 from fixedXOR import fixedXOR
 import cryptoHelpers as ch
 # from wikipedia, alphabetical order
-CHAR_FREQ = (.08167,
-             .01492,
-             .02782,
-             .04253,
-             .12702,
-             .02228,
-             .02015,
-             .06094,
-             .06966,
-             .00153,
-             .00772,
-             .04025,
-             .02406,
-             .06749,
-             .07507,
-             .01929,
-             .00095,
-             .05987,
-             .06327,
-             .09056,
-             .02758,
-             .00978,
-             .02360,
-             .00150,
-             .01974,
-             .00074)
+CHAR_FREQ_SP = (0.0651738, 0.0124248, 0.0217339, 0.0349835,
+                0.1041442, 0.0197881, 0.0158610, 0.0492888,
+                0.0558094, 0.0009033, 0.0050529, 0.0331490,
+                0.0202124, 0.0564513, 0.0596302, 0.0137645,
+                0.0008606, 0.0497563, 0.0515760, 0.0729357,
+                0.0225134, 0.0082903, 0.0171272, 0.0013692,
+                0.0145984, 0.0007836, 0.1918182)
+
+CHAR_FREQ = (0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015,
+             0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749,
+             0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758,
+             0.00978, 0.02360, 0.00150, 0.01974, 0.00074)
+
+FREQ_SW = CHAR_FREQ_SP
 
 def chiSquared(ascii_str):
     ascii_str_low = ascii_str.lower()
     str_len = 0
-    char_counts = [0] * 26
+    char_counts = [0] * 27
 
     for char in ascii_str_low:
-        if ord(char) >= 97 and ord(char) <= 122:
+        if ord(char) == 9 or ord(char) == 10 or ord(char) == 13 or ord(char) == 32:
+            char_counts[26] += 1
+            str_len += 1
+            pass
+        elif ord(char) > 126 or ord(char) < 32:
+            return 1e309
+        elif ord(char) >= 97 and ord(char) <= 122:
             char_counts[ord(char)-97] += 1
             str_len += 1
 
-    exp_counts = [c*str_len for c in CHAR_FREQ]
+    exp_counts = [c*str_len for c in FREQ_SW]
     chi_sq = 0
 
     try:
         for act, exp in zip(char_counts, exp_counts):
             chi_sq += pow(act-exp, 2)/exp
 
-        return chi_sq
+        return chi_sq/str_len
     except ZeroDivisionError:
         return 1e309
 
@@ -56,21 +50,19 @@ def singleByteDecrypt(hex_str):
     mv_string = ''
     mv_key = ''
 
-    extend = int(len(hex_str)/2)
+    extend = len(hex_str)//2
 
     for i in range(0, 128):
         ret_hex_str = fixedXOR(hex_str, format(i, '02x')*extend)
-        try:
-            ret_bytes = bytearray.fromhex(ret_hex_str)
-            ret_ascii_str = ret_bytes.decode('ascii')
-        except (UnicodeDecodeError, TypeError) as e:
-            #print(e)
+        if ch.isHex(ret_hex_str):
+            ret_ascii_str = ch.hexToAscii(ret_hex_str)
+        else:
             continue
-
         chi_sq = chiSquared(ret_ascii_str)
+        #print([ret_ascii_str, chi_sq])
         if chi_sq < min_val:
             min_val = chi_sq
             mv_string = ret_ascii_str
             mv_key = chr(i)
 
-    return mv_string
+    return [mv_string, min_val]
