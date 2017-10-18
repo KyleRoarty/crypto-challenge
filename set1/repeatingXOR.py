2929aab9441308fddce2b1b134e8bf82a2d23c2f
Kyle Roarty <kyleroarty1716@gmail.com>
2017-10-18 15:26:52 -0500

    repeatingXOR: add input string hex/ascii validity
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- set1/repeatingXOR.py
+++ set1/repeatingXOR.py
@@ -5,13 +5,12 @@ from math import ceil
 import cryptoHelpers as ch
 
 def repeatingXOR(str_in, key):
-    b_key = ''
-    hex_str = ''
-
-    b_key = ch.asciiToHex(key)
-    hex_str = ch.asciiToHex(str_in)
+    hex_str = str_in if ch.isHex(str_in) else ch.asciiToHex(str_in)
+    b_key = key if ch.isHex(key) else ch.asciiToHex(key)
 
     ext_key = b_key*ceil(len(hex_str)/len(b_key))
     if len(ext_key) is not len(hex_str):
         ext_key = ext_key[:len(hex_str)-len(ext_key)]
-    print(fixedXOR(hex_str, ext_key))
+
+    return fixedXOR(hex_str, ext_key)
+
