44e593e5f07f86297181cb03f8c0c9d402b7c3a2
Kyle Roarty <kyleroarty1716@gmail.com>
2017-10-18 15:27:15 -0500

    decryptRepeatingXOR: working. Challenge 6 completed.
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- set1/decryptRepeatingXOR.py
+++ set1/decryptRepeatingXOR.py
@@ -3,18 +3,30 @@
 import cryptoHelpers as ch
 import hexto64
 import singleXOR
+import repeatingXOR
 
 def decryptRepeatingXOR(filename):
-    f = open(filename, 'r').read().strip()
-    hex_str = hexto64.b64ToHex(f)
+    f = open(filename, 'r').read().strip().replace('\n','')
+    print(f)
+    hex_str2 = hex_str = hexto64.b64ToHex(f)
 
-    key_len = 0
-    min_hdist = 1e309
+    key_hdist = []
 
     for i in range(2, 41):
-        hdist = ch.hammingDist(hex_str[:i], hex_str[i:2*i])/i
-        if hdist < min_hdist:
-            min_hdist = hdist
-            key_len = i
+        j = i*8
+        hdist = ch.hammingDist(hex_str[:j], hex_str[j:2*j])/i
+        key_hdist.append((i, hdist))
 
+    key_hdist.sort(key=lambda tup: tup[1])
 
+    print(key_hdist[0])
+    split_str = []
+    for i in range(0, key_hdist[0][0]):
+        split_str.append(''.join([hex_str[j:j+2] for j in range(2*i, len(hex_str), 2*key_hdist[0][0])]))
+
+    key = ''
+    for sub_str in split_str:
+        key += singleXOR.singleByteDecrypt(sub_str)[1]
+
+    print(key)
+    return ch.hexToAscii(repeatingXOR.repeatingXOR(hex_str2, key))
