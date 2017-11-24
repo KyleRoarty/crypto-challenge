006c75a5eb18b472601e8ec7150808f90c196d1a
Kyle Roarty <kyleroarty1716@gmail.com>
2017-11-24 16:36:34 -0600

    Working ECB detect function
    Works on files that have multiple lines.
    Checks each line. Each line is in hex format
    
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- ecb.py
+++ ecb.py
@@ -12,3 +12,23 @@ def decryptECB(in_data, isFile, key_bytes):
     cipher = AES.new(key_bytes, AES.MODE_ECB)
 
     return cipher.decrypt(in_bytes)
+
+def detectECB(in_file):
+    f = open(in_file, 'r').read().splitlines()
+
+    f_bytes = [ch.hexToBytes(l) for l in f]
+
+    is_encrypted = []
+
+    for l in f_bytes:
+        split_l = [l[i:i+16] for i in range(0, len(l), 16)]
+        unique = set(split_l)
+
+        if len(split_l) != len(unique):
+            is_encrypted.append(True)
+        else:
+            is_encrypted.append(False)
+
+    return [ch.bytesToHex(f_bytes[i]) for i,v in enumerate(is_encrypted) if v is True]
+
+
